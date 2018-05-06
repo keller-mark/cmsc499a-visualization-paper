08e836a02ff598b0f2a0f5b2485f6eb3a9945e69
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2018-05-05 21:44:03 -0400

    Convert bug

--- figures/convert.sh
+++ figures/convert.sh
@@ -2,5 +2,5 @@ PWD=$(pwd)
 for file in ./*.svg
 do
   # Mac Inkscape path: /Applications/Inkscape.app/Contents/Resources/bin/Inkscape
-  /Applications/Inkscape.app/Contents/Resources/bin/Inkscape -D -z --file="$PWD"/"$file" --export-pdf="$PWD"/"${file%.*}".pdf
+  inkscape -D -z --file="$PWD"/"$file" --export-pdf="$PWD"/"${file%.*}".pdf
 done
